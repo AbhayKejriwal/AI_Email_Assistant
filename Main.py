864c1f9e03e8bf74d82657cd5a22999a7d92b9ed
Abhay Kejriwal <121892598+AbhayKejriwal@users.noreply.github.com>
2024-08-28 13:02:38 +0530

    chore: Update gemini API key configuration in EmailAsst.py

--- Main.py
+++ Main.py
@@ -1,6 +1,6 @@
 import GmailAPI as ml
 import FileManagement as fl
-import EmailAssistant as astn
+import EmailAsst as astn
 import json
 import os
 
@@ -20,24 +20,20 @@ def main():
         print("Email saved as " + str(email['MessageID']) + ".html")
         #save the file link in the email data
         email['MessageFile'] = os.getcwd() + "\\" +str(email['MessageID']) + ".html"
-      # generate prompts for wach email, get output and save it in a new df to later safe to excel
-      prompt = astn.gen_prompt(message)
-      response = astn.generate(prompt)
-      # print(response)
-      json_string = response.split("json")[1]
-      # Remove any leading or trailing whitespace
-      json_string = json_string.strip()[:-3]
+      # get output for each email,  and save it in a new df to later safe to excel
+      response = astn.generate(message)
+      print(response)
       # save the response in a new file with same message id summary as name and .txt extension
       filename = str(email['MessageID']) + "_summary.txt"
       with open(filename, 'w', encoding='utf-8') as file:
-        file.write(json_string)
+        file.write(response)
         print("Summary saved as " + filename)
         email['SummaryFile'] = os.getcwd() + "\\" + filename 
       
       # Parse the JSON string into a dictionary
-      print(json_string)
+      print(response)
       try:
-        res = json.loads(json_string)
+        res = json.loads(response)
         # email['Filtered_Message'] = res['Filtered_Message']
         email['Summary'] = res['Summary']
         email['Category'] = res['Category'] 
